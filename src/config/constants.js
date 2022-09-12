88faa4ee2768e2e9f1e4222a0d3d7597dd6a7224
yanggyuheon <gyuheon0@gmail.com>
2022-09-12 22:51:45 +0900

    API URL 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-yang.herokuapp.com"
+    : "http://loaclhost:8080";
